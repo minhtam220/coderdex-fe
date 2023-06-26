45f6ae4032d54d72014e900ad29861e633a5cb2f
minhtam220 <minhtam220@gmail.com>
2023-06-26 22:04:06 +0700

    issue with getpokemons

--- src/app/config.js
+++ src/app/config.js
@@ -1,2 +1,3 @@
-export const BASE_URL = process.env.REACT_APP_BACKEND_API;
-export const POKEMONS_PER_PAGE = 20;
+export const BASE_URL = "localhost:8000";
+//process.env.REACT_APP_BACKEND_API;
+export const POKEMONS_PER_PAGE = 10;
