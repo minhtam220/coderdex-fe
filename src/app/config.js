28dc25cdb5ba7fe05f1513bab1fd16e160e6b285
minhtam220 <minhtam220@gmail.com>
2023-06-29 20:03:25 +0700

    Update config.js
    add the back end from render

--- src/app/config.js
+++ src/app/config.js
@@ -1,3 +1,3 @@
-export const BASE_URL = "http://localhost:8000";
+export const BASE_URL = "https://pokedex-be-jsnk.onrender.com";
 //process.env.REACT_APP_BACKEND_API;
 export const POKEMONS_PER_PAGE = 12;
